3d1838720320563f307875bacd845f604d98c352
peeyush-jain60 <peeyushjain.jain60@gmail.com>
2020-11-22 13:09:22 +0530

    Updated with AR and firebase

--- app/src/main/java/com/example/foodar/ar_show.java
+++ app/src/main/java/com/example/foodar/ar_show.java
@@ -1,17 +1,92 @@
 package com.example.foodar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class ar_show extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.assets.RenderableSource;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ar_show extends AppCompatActivity {
+    private ModelRenderable renderable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar_show);
         Toast t=Toast.makeText(getApplicationContext(),getIntent().getStringExtra("title"),Toast.LENGTH_SHORT);
         t.show();
+        FirebaseApp.initializeApp(this);
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference modelRef = storage.getReference().child("restaurant/dish/out.glb");
+
+        ArFragment arFragment = (ArFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.arFragment);
+
+        findViewById(R.id.downloadBtn)
+                .setOnClickListener(v -> {
+
+                    try {
+                        File file = File.createTempFile("out", "glb");
+
+                        modelRef.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+
+                                buildModel(file);
+
+                            }
+                        });
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                });
+
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+
+            AnchorNode anchorNode = new AnchorNode(hitResult.createAnchor());
+            anchorNode.setRenderable(renderable);
+            Vector3 vector3 = new Vector3(0.1f,0.1f,0.1f);
+            anchorNode.setLocalScale(vector3);
+            arFragment.getArSceneView().getScene().addChild(anchorNode);
+
+
+        });
+    }
+
+    private void buildModel(File file) {
+
+        RenderableSource renderableSource = RenderableSource
+                .builder()
+                .setSource(this, Uri.parse(file.getPath()), RenderableSource.SourceType.GLB)
+                .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                .build();
+
+        ModelRenderable
+                .builder()
+                .setSource(this, renderableSource)
+                .setRegistryId(file.getPath())
+                .build()
+                .thenAccept(modelRenderable -> {
+                    Toast.makeText(this, "Model built", Toast.LENGTH_SHORT).show();;
+                    renderable = modelRenderable;
+
+                });
+
     }
 }
